e54f9b904f9511e5e9479585e518944bc998930d
Tom Reinhart <tomreinhart72@gmail.com>
2022-08-11 10:47:36 -0400

    closest triplet to target

--- 15.py
+++ 15.py
@@ -1,5 +1,4 @@
 # Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
-import enum
 from typing import List
 class Solution:
     def threeSum_bruteforce(self, nums: List[int]) -> List[List[int]]:
